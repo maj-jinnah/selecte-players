6190d9d2bcd9a9845ee8fa712e339a67cec56a26
maj-jinnah <maj.jinnah@gmail.com>
2022-08-22 23:47:16 +0600

    Body done

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,4 +1,29 @@
+const playerArray = [];
+
+function displayPlayer(playerName){
+    console.log(playerArray);
+    // const playerNamesArea = document.getElementById('players-name');
+
+    // for( let i= 0; i < playerArray.length; i++){
+    //     // console.log(playerArray[i]);
+    //     const playerName = playerArray[i];
+    //     console.log(playerName);
+    // }
+}
+
+
 function addToSelectArea(event){
     const playerNameFromWebsite = event.parentNode.parentNode.children[0].innerText;
-    console.log(playerNameFromWebsite);
+    
+    const playerNameObject = {
+        playerNameFromWebsite: playerNameFromWebsite
+    }
+
+    playerArray.push(playerNameObject);
+
+    const totalPlayerAdded = document.getElementById('total-player-number');
+    totalPlayerAdded.innerText = playerArray.length;
+    // console.log(totalPlayerAdded.innerText);
+
+    displayPlayer(playerArray);
 }
