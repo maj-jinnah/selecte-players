1afa60c0fe503c8a1f3c31c50648c46c87f6c5dc
maj-jinnah <maj.jinnah@gmail.com>
2022-08-22 22:29:10 +0600

    header done

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,1 +1,4 @@
-console.log('ja added/')
+function addToSelectArea(event){
+    const playerNameFromWebsite = event.parentNode.parentNode.children[0].innerText;
+    console.log(playerNameFromWebsite);
+}
