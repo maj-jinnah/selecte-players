b7f60f3faf0c82d460e81a658076cacd3a292609
maj-jinnah <maj.jinnah@gmail.com>
2022-08-23 00:36:30 +0600

    footer done

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,20 +1,39 @@
 const playerArray = [];
 
-function displayPlayer(playerName){
-    console.log(playerArray);
-    // const playerNamesArea = document.getElementById('players-name');
+function displayPlayer(player) {
+    // console.log(player);
+    const playerNamesArea = document.getElementById('players-name');
+    playerNamesArea.innerHTML = '';
 
-    // for( let i= 0; i < playerArray.length; i++){
-    //     // console.log(playerArray[i]);
-    //     const playerName = playerArray[i];
-    //     console.log(playerName);
-    // }
+    for (let i = 0; i < player.length; i++) {
+        // console.log(playerName[i].playerNameFromWebsite);
+        // console.log(playerArray[i]);
+        const playerName = player[i].playerNameFromWebsite;
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th scope="row">${i + 1}</th>
+        <td>${playerName}</td>
+        `;
+
+        playerNamesArea.appendChild(tr);
+
+
+        // const totalPlayerAdded = document.getElementById('total-player-number');
+        // totalPlayerAdded.innerText = playerArray.length;
+        // console.log(totalPlayerAdded.innerText);
+
+        // if(player.length > 5){
+        //     alert("You can't add more players.")
+        // }
+
+    }
 }
 
 
-function addToSelectArea(event){
+function addToSelectArea(event) {
     const playerNameFromWebsite = event.parentNode.parentNode.children[0].innerText;
-    
+
     const playerNameObject = {
         playerNameFromWebsite: playerNameFromWebsite
     }
@@ -23,7 +42,13 @@ function addToSelectArea(event){
 
     const totalPlayerAdded = document.getElementById('total-player-number');
     totalPlayerAdded.innerText = playerArray.length;
-    // console.log(totalPlayerAdded.innerText);
+
+    const number = totalPlayerAdded
+    console.log(number);
+    // if (number > '5') {
+    //     alert("You can't add more players.");
+    //     return;
+    // }
 
     displayPlayer(playerArray);
 }
