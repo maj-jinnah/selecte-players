ff5c2d1ed20bb2493b1a89cf1d5ab7ba093175d0
maj-jinnah <maj.jinnah@gmail.com>
2022-08-23 01:58:19 +0600

    recheck

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,13 +1,27 @@
 const playerArray = [];
 
 function displayPlayer(player) {
-    // console.log(player);
+
+
+                     // check
+    // const numberArea = document.getElementById('total-player-number');
+    // const numberString = numberArea.innerText
+    // const number = parseInt(numberString);
+
+    // // console.log(number);
+    // if (number > 5) {
+    //     alert("You can't add more player");
+    //     return;
+    // }
+    // playerNumberCheck();
+
+
+
+    
     const playerNamesArea = document.getElementById('players-name');
     playerNamesArea.innerHTML = '';
 
     for (let i = 0; i < player.length; i++) {
-        // console.log(playerName[i].playerNameFromWebsite);
-        // console.log(playerArray[i]);
         const playerName = player[i].playerNameFromWebsite;
 
         const tr = document.createElement('tr');
@@ -18,20 +32,42 @@ function displayPlayer(player) {
 
         playerNamesArea.appendChild(tr);
 
+        // const numberArea = document.getElementById('total-player-number');
+        // const numberString = numberArea.innerText
+        // const number = parseInt(numberString);
 
-        // const totalPlayerAdded = document.getElementById('total-player-number');
-        // totalPlayerAdded.innerText = playerArray.length;
-        // console.log(totalPlayerAdded.innerText);
-
-        // if(player.length > 5){
-        //     alert("You can't add more players.")
+        // // console.log(number);
+        // if (number > 5) {
+        //     alert("You can't add more player");
+        //     break;
         // }
 
     }
+
+    // return;
+
 }
 
 
 function addToSelectArea(event) {
+
+
+    //              // check
+    const numberArea = document.getElementById('total-player-number');
+    const numberString = numberArea.innerText
+    const number = parseInt(numberString);
+
+    // console.log(number);
+    if (number >= 5) {
+        alert("You can't add more player");
+        return;
+    }
+    // playerNumberCheck();
+    
+    
+    
+    // document.getElementById('select-btn').disabled = true;
+
     const playerNameFromWebsite = event.parentNode.parentNode.children[0].innerText;
 
     const playerNameObject = {
@@ -43,8 +79,9 @@ function addToSelectArea(event) {
     const totalPlayerAdded = document.getElementById('total-player-number');
     totalPlayerAdded.innerText = playerArray.length;
 
-    const number = totalPlayerAdded
-    console.log(number);
+    // const number = totalPlayerAdded
+    // console.log(number);
+
     // if (number > '5') {
     //     alert("You can't add more players.");
     //     return;
---
+++ javaScript/comon.js
@@ -0,0 +1,11 @@
+// function playerNumberCheck() {
+//     // check
+//     const numberArea = document.getElementById('total-player-number');
+//     const numberString = numberArea.innerText
+//     const number = parseInt(numberString);
+
+//     if (number >= 5) {
+//         alert("You can't add more player");
+//         return;
+//     }
+// }
